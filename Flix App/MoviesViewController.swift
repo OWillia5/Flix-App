4f6951b82ff596e77d4471f94075ee20ac6d7897
Opeoluwa Williams <davwilliams6@gmail.com>
2020-03-11 16:23:55 -0500

    Changes to the Flix App

--- Flix App/MoviesViewController.swift
+++ Flix App/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource , UITableViewDelegate {
 
@@ -65,7 +66,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource , UITableVie
         cell.movieTitle.text = title
         cell.movieDescription.text = description
         
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
         
+        cell.movieImage.af.setImage(withURL: posterUrl)
+    
         return cell
     }
     
