fa7858c9efa0d86df70709db45d79b3279871dd4
Opeoluwa Williams <davwilliams6@gmail.com>
2020-04-21 11:32:04 -0700

    Added Details feature and Superhero section

---
+++ Flix App/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix App
+//
+//  Created by user169361 on 4/21/20.
+//  Copyright © 2020 davwilliams6@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
